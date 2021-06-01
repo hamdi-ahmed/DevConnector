30babaab5b69cfd34bd18d91d78f61f5d35babab
hamdi-ahmed <hamdia108@gmail.com>
2021-06-01 12:10:12 +0200

    profile APIS done and also  git repos from github

--- backend/controller/profileController.js
+++ backend/controller/profileController.js
@@ -1,6 +1,11 @@
+const { compareSync } = require('bcryptjs')
+const request = require('request')
+//const config = require('config')
+const expressAsyncHandler = require('express-async-handler')
 const asyncHandler = require('express-async-handler')
 const Profile = require('../models/profileModel')
 const User = require('../models/userModel')
+const { json } = require('express')
 
 // @route     GET api/profile/me
 // @desc      Get user profile
@@ -152,11 +157,165 @@ const getProfileByUserId = asyncHandler(async (req, res) => {
     }
 })
 
+
+// @route     DELETE api/profile/
+// @desc      Delete user and profiles
+// @access    private route
+const deleteProfilesAndUsers = asyncHandler(async (req, res) => {
+    try {
+        await Profile.findOneAndRemove({ user: req.user.id })
+        await User.findOneAndRemove({ _id: req.user.id })
+        res.json('Deleted')
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Server error')
+    }
+})
+
+// @route     PUT api/profile/experience
+// @desc      Add experience
+// @access    private route
+const addExperience = asyncHandler(async (req, res) => {
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+
+    //Assign the values to a new object 
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    const profile = await Profile.findOne({ user: req.user.id })
+    if (profile) {
+        profile.experience.unshift(newExp)
+        await profile.save()
+        res.json(profile)
+    } else {
+        res.status(404)
+        throw new Error('Profile not found')
+    }
+})
+
+
+// @route     DELETE api/profile/experience
+// @desc      Delete experience
+// @access    private route
+const deleteExperience = asyncHandler(async (req, res) => {
+    try {
+        //find profile
+        const profile = await Profile.findOne({ user: req.user.id })
+        //find the exp that i want to delete
+        const profileExp = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id)
+        //deleted 
+        profile.experience.splice(profileExp, 1)
+        await profile.save()
+        res.json(profile)
+
+    } catch (err) {
+        console.error('Server Error')
+        res.status(500)
+    }
+})
+
+// @route     PUT api/profile/education
+// @desc      Add education
+// @access    private route
+const addEducation = asyncHandler(async (req, res) => {
+    const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+
+    const addEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    }
+    const profile = await Profile.findOne({ user: req.user.id })
+    if (profile) {
+        profile.education.unshift(addEdu)
+        await profile.save()
+        res.json(profile)
+    } else {
+        res.status(404)
+        throw new Error('Profile not found')
+    }
+})
+
+
+// @route     DELETE api/profile/education/edu_id
+// @desc      Delete education
+// @access    private route
+const deleteEducation = asyncHandler(async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id })
+        const deleteEdu = profile.education
+            .map(item => item.id)
+            .indexOf(req.params.edu_id)
+        profile.education.splice(deleteEdu, 1)
+        await profile.save()
+        res.json(profile)
+    } catch (err) {
+        console.error(err)
+        res.status(500)
+    }
+})
+
+// @route     GET api/profile/github/username
+// @desc      Get all repo from github
+// @access    public route
+const gitAllRepoFromGithub = asyncHandler(async (req, res) => {
+    const options = {
+        uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc
+        &clientId=${process.env.GITHUB_CLIENT_ID}$clientSecret=${process.env.GITHUB_CLIENT_SECRET}
+        `,
+        method: "GET",
+        headers: { 'user-agent': 'node.js' }
+    }
+    request(options, (error, response, body) => {
+        if (error) console.error(error)
+
+        if (response.statusCode !== 200) {
+            return res.status(404).json('No github profile found')
+        }
+        res.json(JSON.parse(body))
+    })
+})
+
 module.exports = {
     registerUserProfile,
     getProfile,
     updateProfile,
     getUserProfile,
     getUserProfiles,
-    getProfileByUserId
+    getProfileByUserId,
+    deleteProfilesAndUsers,
+    addExperience,
+    deleteExperience,
+    addEducation,
+    deleteEducation,
+    gitAllRepoFromGithub
 }
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,5 +1,18 @@
 const express = require('express')
-const { getUserProfile, registerUserProfile, updateProfile, getProfile, getUserProfiles, getProfileByUserId } = require('../controller/profileController')
+const {
+    getUserProfile,
+    registerUserProfile,
+    updateProfile,
+    getProfile,
+    getUserProfiles,
+    getProfileByUserId,
+    deleteProfilesAndUsers,
+    addExperience,
+    deleteExperience,
+    addEducation,
+    deleteEducation,
+    gitAllRepoFromGithub
+} = require('../controller/profileController')
 const protect = require('../middleWares/authMiddleware')
 
 const router = express.Router()
@@ -8,7 +21,18 @@ const router = express.Router()
 router.route('/')
     .post(protect, registerUserProfile)
     .get(getUserProfiles)
+    .delete(protect, deleteProfilesAndUsers)
 
+router.route('/experience').put(protect, addExperience)
+
+router.route('/education').put(protect, addEducation)
+
+router.route('/experience/:exp_id').delete(protect, deleteExperience)
+
+
+router.route('/education/:edu_id').delete(protect, deleteEducation)
+
+router.route('/github/:username').get(gitAllRepoFromGithub)
 
 router.route('/me')
     .get(protect, getUserProfile)
