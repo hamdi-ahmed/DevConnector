ad629d23ab8f63614eba8cb46fd461d80243adea
hamdi-ahmed <hamdia108@gmail.com>
2021-06-02 19:27:40 +0200

    adding register and login page (UI)

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,10 +1,48 @@
-import React from 'react'
-
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+    const { email, password } = formData
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log('success');
+    }
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+            <form onSubmit={handleSubmit} className="form" action="dashboard.html">
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                        autoComplete="off"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        name="password"
+                        autoComplete="off"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
     )
 }
 
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,10 +1,85 @@
-import React from 'react'
-
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+    const { name, email, password, password2 } = formData
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (password !== password2) {
+            console.log('Password does not match ');
+        } else {
+            console.log(formData)
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form
+                className="form"
+                onSubmit={handleSubmit}
+            >
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={e => onChange(e)}
+                        name="name"
+                        required
+                        autoComplete="off"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        name="email"
+                        autoComplete="off"
+                    />
+                    <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                      Gravatar email</small
+                    >
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength="6"
+                        autoComplete="off"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        value={password2}
+                        onChange={e => onChange(e)}
+                        minLength="6"
+                        autoComplete="off"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
 
